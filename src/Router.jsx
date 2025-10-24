4ad1db64b5a665e3ab78d51622b13f145e18f636
akhildk <akhildemoacc@gmail.com>
2025-10-24 13:09:16 +0530

    new1

--- src/Router.jsx
+++ src/Router.jsx
@@ -1,19 +1,17 @@
-import React from 'react'
-import { BrowserRouter, Route, Routes } from 'react-router-dom'
-import Home from "./Home"
-import Login from './pages/Login'
-import Details from './pages/Details'
+import React from 'react';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Products from './pages/Products';
+import Login from './pages/Login';
+import Details from './pages/Details';
 
-export default function Router() {
+export default function AppRouter() {
   return (
-   <>
-   <BrowserRouter>
-   <Routes>
-    <Route path="/" element={<Home/>}/>
-     <Route path="/login" element={<Login/>}/>
-     <Route path="/details/:id" element={<Details/>}/>
-   </Routes>
-   </BrowserRouter>
-   </>
-  )
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Products />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/details/:id" element={<Details />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
--- src/pages/Products.jsx
+++ src/pages/Products.jsx
@@ -1,20 +1,20 @@
 import React, { useEffect, useState } from "react";
-import Navbar from "./component/Navbar";
-import Card from "./component/Card";
+import Navbar from "../component/Navbar";
+import Card from "../component/Card";
 
-function Home() {
+function Products() {
   const [products, setProducts] = useState([]);
 
   const fetchStoreProducts = async () => {
     try {
-      const res = await fetch("https://fakestoreapi.com/products");
+      const res = await fetch("http://localhost:7070/user");
       const data = await res.json();
       console.log("Fake store data:", data);
       setProducts(data);
     } catch (error) {
       console.error("Error fetching products:", error);
-    } 
-  };0
+    }
+  };
 
   useEffect(() => {
     fetchStoreProducts();
@@ -22,18 +22,16 @@ function Home() {
 
   return (
     <div className="w-full min-h-screen bg-gray-100">
-    
       <div className="w-full h-[12vh]">
         <Navbar />
       </div>
 
-     
       <div className="flex flex-wrap justify-center gap-6 p-6">
         {products.map((item) => (
           <Card
-          id={item.id}
             key={item.id}
-            title={item.title}
+            id={item.id}
+            title={item.name}
             price={item.price}
             image={item.image}
           />
@@ -43,4 +41,4 @@ function Home() {
   );
 }
 
-export default Home;
+export default Products;
